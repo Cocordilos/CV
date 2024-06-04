8150335812a1dd864fd327e966f23d7d8886ba37
Cocordilos <maksimka.goreli@GMAIL.COM>
2024-06-04 15:20:03 +0400

    Add files via upload

---
+++ html/js/script.js
@@ -0,0 +1,7 @@
+var button = document.querySelector('button');
+
+
+button.addEventlistener('click', function() {
+console.log('log:Выведи этот текст если кнопка нажмется');
+alert('бить кого-либо плохо!')
+})
